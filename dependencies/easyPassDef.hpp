4b7dec1212ae087be0878c1312996e4b692561fc
Cigan Oliviu David <43005157+CiganOliviu@users.noreply.github.com>
2020-05-15 22:06:26 +0300

    Update EasyPass-WS

--- dependencies/easyPassDef.hpp
+++ dependencies/easyPassDef.hpp
@@ -112,12 +112,12 @@ namespace easyPassDefinitions {
     virtual ~randomGenerator () {}
   };
 
-  class checkAndSupport {
+  class lowLevelSupport {
   private:
     errorsHandler __handler__;
-
+    
   public:
-    checkAndSupport () {}
+    lowLevelSupport () {}
 
     template <class Type> void readOneDimensionalArray (oneDimensionalArrayType<Type> ODAObject);
     template <class Type> void readDynamicFileDimensionalArray (char * fileName, oneDimensionalArrayType<Type> ODAObject);
@@ -131,6 +131,17 @@ namespace easyPassDefinitions {
 
     template <class Type> void readTree (binaryTreeType<Type> *& root);
     template <class Type> void putsTree (binaryTreeType<Type> * root);
+
+    virtual ~lowLevelSupport () {}
+  };
+
+
+  class checkAndSupport {
+  private:
+    errorsHandler __handler__;
+
+  public:
+    checkAndSupport () {}
 
     template <class Type> Type returnTheMaximumParameter (limits<Type> limitsObject);
     template <class Type> Type returnTheMinimumParameter (limits<Type> limitsObject);
